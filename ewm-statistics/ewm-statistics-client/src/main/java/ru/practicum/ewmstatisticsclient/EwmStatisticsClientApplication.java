ffc223dc6e483a9d5027a6dc7e258ddaa72e6691
living221 <living221@gmail.com>
2023-05-29 19:09:18 +0700

    remove inner git repos

---
+++ ewm-statistics/ewm-statistics-client/src/main/java/ru/practicum/ewmstatisticsclient/EwmStatisticsClientApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.ewmstatisticsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwmStatisticsClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EwmStatisticsClientApplication.class, args);
+    }
+
+}
